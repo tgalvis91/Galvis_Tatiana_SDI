a0f2ebf846df5f7f4c649f5cbef17c7569d95261
Tgalvis91 <tgalvis91@gmail.com>
2015-09-17 20:54:58 -0700

    Conditionals Submit
    Conditionals Submit

---
+++ Galvis_Tatiana_Conditionals/Galvis_Tatiana_Conditionals_Assignment.js
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <Conditional></Conditional>
+</head>
+<body>
+
+int 12-10
+
+int 12/2
+
+("12-10 = "2);
+
+("12/2= "6);
+
+
+bored<false
+
+if(bored)
+
+willReadABook(){
+
+}else{
+
+willPlayWithDog(){
+
+play></false>
+
+boolean isRelaxed = true;
+
+String mood = "";
+
+if (isRelaxed == true)
+{
+mood = "I'm Relaxed!";
+}
+else
+{
+mood = "I'm Stressed!";
+}
+boolean b;
+
+b = 3 > 2 && 2 < 9; // b is true
+
+b = 2 > 3 && 9 < 2; // b is now false
+</body>
+</html>
